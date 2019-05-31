23659b8ff310a4b26a3121512c08fe0f699ab85c
João Paulo <jpaulo.oliveira@gmail.com>
2019-05-31 02:10:30 +0100

    .

---
+++ MoonMembers/Controllers/MembersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoonMembers.Models;
+
+namespace MoonMembers.Controllers
+{
+    public class MembersController : Controller
+    {
+        // GET: Members
+        public ActionResult Index()
+        {
+            moonMembersEntities DB = new moonMembersEntities();
+
+            List<members> members = DB.members.ToList();
+
+
+            return View(members);
+        }
+    }
+}
